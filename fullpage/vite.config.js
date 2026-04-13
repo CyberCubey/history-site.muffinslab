dd294ce5f2eca2b19b8f95413b6a553481361848
CyberCubey <Jonathan.u.hansen@gmail.com>
2026-04-13 10:59:35 +0200

    change

--- fullpage/vite.config.js
+++ fullpage/vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+       manifest: {
+	"name": "Histori",
+	"short_name": "Hist",
+	"start_url": "./index.html",
+	"id": "./index.html",
+
+	"display": "standalone",
+	"background_color": "#ffffff",
+	"theme_color": "#ffffff",
+	"orientation": "portrait-primary",
+
+    "icons": [{
+      "src": "./src/assets/web-app-manifest-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "./src/assets/web-app-manifest-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+    }
+  
+})]})
